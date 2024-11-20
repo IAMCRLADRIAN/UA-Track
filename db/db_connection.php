4f3c05505dba5e947d7ac1ad99b66ccbef252121
IAMCRLADRIAN <143055462+IAMCRLADRIAN@users.noreply.github.com>
2024-11-20 22:42:58 +0800

    Update db_connection.php

--- db/db_connection.php
+++ db/db_connection.php
@@ -1,16 +1,11 @@
 <?php
-
-$host = 'localhost';        
-$dbname = 'ClinicDashboard'; 
-$username = 'root';         
-$password = '';             
-
-
+$dsn = 'mysql:host=localhost;dbname=ClinicDashboard';
+$username = 'root';
+$password = 'admin123';
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-   
+    $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    die("Database connection failed: " . $e->getMessage());
 }
 ?>
